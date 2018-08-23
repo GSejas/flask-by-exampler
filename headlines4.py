019cf255b6001858714bbbe3418b276f17a8139b
GSejas <jsequeira03@gmail.com>
2018-08-22 23:24:56 -0600

    + cookies support

--- headlines4.py
+++ headlines4.py
@@ -1,5 +1,6 @@
 import feedparser
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
+import datetime
 
 app = Flask(__name__)
 RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
@@ -7,17 +8,40 @@ RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
             'fox':'http://feeds.foxnews.com/foxnews/latest',
             'iol':'http://www.iol.co.za/cmlink/1.640'}
 
-
+DEFAULTS = {'publication':'bbc',
+            'city': 'London,UK',
+            'currency_from':'GBP',
+            'currency_to':'USD'
+}
 
 @app.route("/")
-def get_news():
-    query = request.args.get("publication")
+def home():
+    # get customised headlines, based on user input or default
+    publication = get_value_with_fallback("publication")
+    articles = get_news(publication)
+
+    # feed = feedparser.parse(RSS_FEEDS[publication])
+
+    # save cookies and return templates
+    response = make_response(render_template("template.html", articles=articles))
+    expires = datetime.datetime.now() + datetime.timedelta(days=365)
+    response.set_cookie("publication", publication, expires=expires)
+    return response
+
+def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
-        publication = "bbc"
+        publication = DEFAULTS["publication"]
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    return render_template("template.html", articles=feed['entries'])
+    return feed['entries']
+
+def get_value_with_fallback(key):
+    if request.args.get(key):
+        return request.args.get(key)
+    if request.cookies.get(key):
+        return request.cookies.get(key)
+    return DEFAULTS[key]
 
 if __name__ == "__main__":
     app.run(port=5003, debug=True)
